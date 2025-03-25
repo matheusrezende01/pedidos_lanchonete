bcc69b4e04c8d0bd8a453b93d00410dc8c2fae3c
matheusrezende01 <matheusfernandesrezende16@gmail.com>
2025-03-25 15:40:02 -0300

    cadastrando os clientes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Livewire\ProdutoForm;
+use App\Livewire\ProdutoList;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/produtos/create', ProdutoForm::class);
+Route::get('/produtos', ProdutoList::class);
