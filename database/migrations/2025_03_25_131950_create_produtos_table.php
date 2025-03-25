795f5ca2cf54efe61f7fc3c4eb03cd62d3e3f5d4
matheusrezende01 <matheusfernandesrezende16@gmail.com>
2025-03-25 10:32:29 -0300

    model produto

---
+++ database/migrations/2025_03_25_131950_create_produtos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome')->nullable(false); 
+        	$table->string('ingredientes')->nullable(false); 
+        	$table->string('valor')->nullable(false);
+        	$table->string('imagem')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
